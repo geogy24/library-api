26aab65c4cc4d39d0e143b1306575202b67ff848
Jorge Díaz <joredudiaz@gmail.com>
2019-12-05 18:57:09 -0500

    Test books.controller#index

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,5 +1,5 @@
 var models = require('../models');
-var booksSerializer = require('../serializers/booksSerializer');
+var booksSerializer = require('../serializers/books.serializer');
 
 module.exports.index = (_request, response, _next) => {
   models.Book.findAll().then((items) => response.json(booksSerializer.serialize(items)));

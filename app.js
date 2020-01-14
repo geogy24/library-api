92805141612917c0acb6e748fe2cd17e2df99c87
geogy <joredudiaz@gmail.com>
2020-01-13 20:06:29 -0500

    docs(subjects): documentations of subjects endpoints

--- app.js
+++ app.js
@@ -9,14 +9,51 @@ var subjectsRouter = require( "./routes/subjects" );
 
 var app = express(  );
 
+const expressSwagger = require( "express-swagger-generator" )( app );
+
 app.use( logger( "dev" ) );
 app.use( express.json(  ) );
 app.use( express.urlencoded( { extended: false } ) );
 app.use( cookieParser(  ) );
 app.use( express.static( path.join( __dirname, "public" ) ) );
 
+var v1 = express.Router();
+app.use( "/v1", v1 );
+
+var admin = express.Router();
+v1.use( "/admin", admin );
+
 app.use( "/", indexRouter );
 app.use( "/books", booksRouter );
-app.use( "/subjects", subjectsRouter );
+admin.use( "/subjects", subjectsRouter );
+
+let options = {
+	swaggerDefinition: {
+		info: {
+			description: "This is an API for library apps",
+			title: "Library",
+			version: "1.0.0",
+		},
+		host: "localhost:3000",
+		basePath: "/v1",
+		produces: [
+			"application/json",
+			"application/xml"
+		],
+		schemes: [ "http", "https" ],
+		securityDefinitions: {
+			JWT: {
+				type: "apiKey",
+				in: "header",
+				name: "Authorization",
+				description: "",
+			}
+		}
+	},
+	basedir: __dirname, //app absolute path
+	files: [ "./routes/**/*.js" ] //Path to the API handle folder
+};
+
+expressSwagger( options );
 
 module.exports = app;
